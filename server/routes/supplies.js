4d3bbfaf9d8c959b9f4314c0dbf21a91c978ddd3
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 16:59:50 -0500

    Refactor supplies route to /

--- server/routes/supplies.js
+++ server/routes/supplies.js
@@ -3,7 +3,7 @@ import pool from '../db.js'
 
 const router = express.Router();
 
-router.get('/api/supplies', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM supplies');
         res.status(200).json(result.rows);
