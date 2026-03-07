e2fecd25fb0194b9e0d2f477a530b73bec9a404f
Paige Li <itspaige.nli@gmail.com>
2026-03-07 08:00:09 -0800

    Initial Commit and Project Setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from us!" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
