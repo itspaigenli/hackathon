142508b181b916b4f6c47dcb47a1464be8dee1a2
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 17:03:17 -0500

    Add supplies delete /:id route

--- server/routes/supplies.js
+++ server/routes/supplies.js
@@ -13,4 +13,22 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            'DELETE FROM supplies WHERE id=$1 RETURNING *', 
+            [id]
+        );
+
+        if (result.rows.length === 0) return res.status(404).json({ error: 'This supply does not exist!'});
+
+        res.status(200).json({ message: `The supply "${result.rows[0].name}" was deleted!` })
+    } catch (error) {
+        console.error('Error with deleting supply: , error');
+        res.status(500).json({ error: 'Error! Could not delete supply' });
+    }
+});
+
 export default router;
