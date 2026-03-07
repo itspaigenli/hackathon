afad29b2ba59927c08ee35a9188571547b59325b
Paige Li <itspaige.nli@gmail.com>
2026-03-07 10:22:11 -0800

    Add database connection test query

--- server/db.js
+++ server/db.js
@@ -11,4 +11,14 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
+// Database connection test query
+pool
+  .query("SELECT NOW()")
+  .then((res) => {
+    console.log("Database connected:", res.rows[0]);
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
+
 export default pool;
