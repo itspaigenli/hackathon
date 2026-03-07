28d924e9247f2c725dd794d87c78d50aeccf2447
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 16:12:08 -0500

    refactor error message in getAllSupplies function with a clear message

--- server/routes/supplies.js
+++ server/routes/supplies.js
@@ -5,6 +5,6 @@ export const getAllSupplies = async (req, res) => {
         const result = await pool.query('SELECT * FROM supplies');
         res.status(200).json(result.rows);
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({ error: 'Error! Could not get supplies!' });
     }
 }
