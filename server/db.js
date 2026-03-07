5f86a432b9e52b33f3d6b7fa6904dffcc280879b
Paige Li <itspaige.nli@gmail.com>
2026-03-07 10:29:36 -0800

    Update db.js to contain code to run SSL parameters for Supabase

--- server/db.js
+++ server/db.js
@@ -1,5 +1,3 @@
-// Database configuration file
-
 import pg from "pg";
 import dotenv from "dotenv";
 
@@ -9,6 +7,18 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool
+  .query("SELECT NOW()")
+  .then((res) => {
+    console.log("Database connected:", res.rows[0]);
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err.message);
+  });
 
 export default pool;
