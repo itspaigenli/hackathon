24c2624a8107c5feef1943a71afa9ccb8f0fe8f5
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 16:16:20 -0500

    Add console.error error logging message

--- server/routes/supplies.js
+++ server/routes/supplies.js
@@ -5,6 +5,7 @@ export const getAllSupplies = async (req, res) => {
         const result = await pool.query('SELECT * FROM supplies');
         res.status(200).json(result.rows);
     } catch (error) {
+        console.error('Error with getting supplies: ', error);
         res.status(500).json({ error: 'Error! Could not get supplies!' });
     }
 }
