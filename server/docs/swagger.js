36802bb4945b49ad4cb4144085816ffea82b9146
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-07 21:04:11 -0500

    refactor apis routes path in options section

--- server/docs/swagger.js
+++ server/docs/swagger.js
@@ -15,7 +15,7 @@ const swaggerDefinition = {
 
 const options = {
     swaggerDefinition,
-    apis: ['../routes/*.js'],
+    apis: ['./routes/*.js'],
 };
 
 const swaggerSpecs = swaggerJSDoc(options);
