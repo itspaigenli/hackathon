d5a80475a10e37e434fc570dee35dde0144b5abc
Siyi Ji <sylviajsyy@gmail.com>
2026-03-07 19:02:20 -0600

    GET supplies in a safehouse

--- server/routes/safehouses.js
+++ server/routes/safehouses.js
@@ -53,10 +53,34 @@ router.get('/:id/survivors', async (req, res) => {
             count: result.rows.length,
             survivors: result.rows
         });
-        
+
     } catch (error) {
         console.error("Error fetching survivors for safehouse:", error);
         res.status(500).json({ error: "Failed to fetch survivors" });
+    }
+})
+
+// GET supplies in a safe house
+router.get('/:id/supplies', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await pool.query (
+            `SELECT * FROM supplies
+             WHERE safehouse_id = $1
+             ORDER BY id ASC`,
+             [id]
+        )
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Supplies not found" });
+        }
+
+        res.status(200).json(result.rows);
+        
+    } catch (error) {
+        console.error("Error fetching supplies for safehouse:", error);
+        res.status(500).json({ error: "Failed to fetch supplies" });
     }
 })
 
